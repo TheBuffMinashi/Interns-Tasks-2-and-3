69b041ee3d03e79a2f2e0da002f43209a33ff2ae
Hamidreza Ebrahimi <hamidrezaebrahimi67@gmail.com>
2023-06-19 05:57:47 +0330

    Connecting the front-end to the API and further cleaning up the code

--- task3/back/laptop/views.py
+++ task3/back/laptop/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
-import os
-import environ
+import os, environ
+from rest_framework.parsers import JSONParser
 
 def index(request):
  return HttpResponse("message")
 
 
-@api_view()
-def hello_world(request):
-  
-    return Response({"message": "hello_world"})
+@api_view(['POST'])
+@parser_classes([JSONParser])
+def hello_world(request, format=None):
+    question= request.data['question']
+
+    return Response({"message": question})
